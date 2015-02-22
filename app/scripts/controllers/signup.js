de7912c698c3cea4088b03b9276b96a3a4d4b576
DmitriyKab <dkabardinov@gmail.com>
2015-02-22 22:50:08 +0500

    add unit test for SignupCtrl controller

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -8,12 +8,21 @@
  * Controller of the fmAppApp
  */
 angular.module('fmAppApp')
-  .controller('SignupCtrl', function ($scope) {
+  .controller('SignupCtrl', function ($scope, $http) {
     var user = {};
-    $scope.signup = function () {
+    user.name = $scope.name;
+    user.email = $scope.email;
+    user.password = $scope.password;
 
+    $scope.signup = function () {
+      $http.post('/api/signup', user).success(function(data, status) {
+        localStorage.setItem('authToken', data.token);
+      }).error(function(data, status) {
+
+      });
     };
-    $scope.user = user;
+
+    //$scope.user = user;
     $scope.logState = function (form) {
       console.log(form);
     }
--- test/spec/controllers/signup.js
+++ test/spec/controllers/signup.js
@@ -1,22 +1,48 @@
 'use strict';
 
 describe('Controller: SignupCtrl', function () {
-
-  // load the controller's module
   beforeEach(module('fmAppApp'));
 
-  var SignupCtrl,
-    scope;
+  var ctrl,
+    scope, mockBackend;
 
-  // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
-    scope = $rootScope.$new();
-    SignupCtrl = $controller('SignupCtrl', {
-      $scope: scope
+  describe('method signUp', function() {
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope) {
+      mockBackend = _$httpBackend_;
+      scope = $rootScope.$new();
+    }));
+
+    it('should send POST request with user data to the server',
+      inject (function($controller) {
+        scope.name = 'John';
+        scope.email = 'some@email';
+        scope.password = 'somePass';
+        ctrl = $controller('SignupCtrl', { $scope: scope });
+        mockBackend.expectPOST('/api/signup',
+          {name:'John', email: 'some@email', password: 'somePass' }).respond(200, '');
+        scope.signup();
+        mockBackend.flush();
+      }));
+
+    it('should set token from response to localstorage in case of success', inject(function($controller) {
+      localStorage.removeItem('authToken');
+      var token = 'asdfasfgegr';
+      expect(localStorage.getItem('authToken')).toBeNull();
+      ctrl = $controller('SignupCtrl', {$scope: scope});
+      mockBackend.expectPOST('/api/signup').respond(200, {token: token});
+      scope.signup();
+      mockBackend.flush();
+      expect(localStorage.getItem('authToken')).toBe(token);
+    }));
+
+    it('should change location to / in case  of success', function() {
+
     });
-  }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.user).toEqual({});
+    it('should assign status "error" to the scope in case of error', function() {
+
+    });
+
   });
 });
