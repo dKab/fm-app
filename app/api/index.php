f93ce56172d65eb93de13a655f1f5ba7f0f25d47
DmitriyKab <dkabardinov@gmail.com>
2015-03-15 13:40:06 +0500

    123

--- app/api/index.php
+++ app/api/index.php
@@ -15,7 +15,7 @@ ActiveRecord\Config::initialize(function($cfg)
      $cfg->set_model_directory('models');
      $cfg->set_connections([
         'development' => 'mysql://finance_manager:123456@localhost/finance_manager',
-        'production' => ''
+        'production' => getenv('DATABASE_URL')
         ]);
       $cfg->set_default_connection('development');
  });
